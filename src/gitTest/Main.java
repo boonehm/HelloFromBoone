9877084dad7d916880748324e7230d1c2f3109c2
Hannah Rogers <rog19008@byui.edu>
2020-05-14 15:23:26 -0600

    Hello from Hannah

--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Boone.");
 	System.out.println("Hello Boone, my name is Kyle.");
+	System.out.println("Hello Boone, my name is Hannah.");
     }
 }
